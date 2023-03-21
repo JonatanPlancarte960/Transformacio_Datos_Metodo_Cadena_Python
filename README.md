e657834d447d7c4adc5418c8c9646b9ac7069c1e
Jonatan Plancarte <jonatanplancarte960is@gmail.com>
2023-03-21 13:12:49 -0600

    Transformacion de Datos Cadena

---
+++ README.md
@@ -0,0 +1,3 @@
+# Transformacion de Texto con el Metodo Cadena en Python
+En este codigo se usa la transformacion de texto usando el metodo cadena de caracteres en python
+La cadena se puede modificar segun las necesidades del proyecto en el que se use
---
+++ Transformacion_de_Texto_Metodo_Cadena.py
@@ -0,0 +1,9 @@
+texto = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
+
+oraciones = texto.split('. ')
+
+print(oraciones)
+
+for oracion in oraciones:
+    if 'temperature' in oracion:
+        print('Esta Oracion Contiene la Palabra "Temperature" : ' + oracion)
